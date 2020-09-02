8e3942ab2c55844a32692c455c712650601fff23
Milton Lizardo <malkonix@gmail.com>
2020-09-02 11:24:24 -0400

    Allways RTFM!

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const faker = require('faker')
 const bodyParser = require('body-parser')
 const history = require('connect-history-api-fallback')
 const cors = require('cors')
-const port = 3000 || process.env.PORT
 // const Read = require('./app/middlewares/readgana')
 
 const app = express()
@@ -16,11 +15,11 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json()); // Send JSON responses
 app.use(cors());
-app.use(history());
 
 if (process.env.NODE_ENV !== "production") {
     const logger = require('morgan')
     app.use(logger('combined')); // Log requests to API using morgan
+    app.use(history());
 }
 
 app.get('/', (req, res) => res.json({
@@ -42,6 +41,6 @@ app.get('/gps', (req, res) => {
 
 router(app)
 
-app.listen(port, () => {
-    console.log('Server is listening on port ', port)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is listening on port', process.env.PORT || 3000)
 })
