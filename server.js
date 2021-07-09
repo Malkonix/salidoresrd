1a03550555c7141c9bc6d04f7cd2479dd7f60055
Milton Lizardo <malkonix@gmail.com>
2021-07-09 00:42:41 -0400

    Back to YARN

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json()); // Send JSON responses
 app.use(cors());
-app.use(logger('dev'))
+//app.use(logger('dev'))
 
 app.get('/', (req, res) => res.json({
     // id: req.id,
